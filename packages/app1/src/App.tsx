3639fb2f9c54663013e1404e2e0d09825c995a16
prince tanwar <prince.t@simformsolutions.com>
2023-12-19 18:01:36 +0530

    init commit

---
+++ packages/app1/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const App = () => (
+  <fieldset>
+    <legend>MFE</legend>
+    <h1>
+      <p>App 1</p>
+    </h1>
+  </fieldset>
+);
+
+export default App;
---
+++ packages/app2/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const App = () => (
+  <fieldset>
+    <legend>MFE</legend>
+    <h1>
+      <p>App 2</p>
+    </h1>
+  </fieldset>
+);
+
+export default App;
